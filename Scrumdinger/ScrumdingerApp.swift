47af9c657851275b9d06563f8784cc9db4c163b2
lyubovmenshikova <dyachenko.lubov@yandex.ru>
2022-06-27 23:25:25 +0300

    add some logic of time screen

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lyubov Menshikova on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
